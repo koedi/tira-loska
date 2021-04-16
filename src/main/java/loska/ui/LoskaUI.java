84c60e591a3806d92516b4f98da5b54c1da995d4
koedi <kari.pietikainen@gmail.com>
2021-04-16 07:40:33 +0300

    maze buttons are working, size parameters TODO

--- src/main/java/loska/ui/LoskaUI.java
+++ src/main/java/loska/ui/LoskaUI.java
@@ -9,6 +9,7 @@ import javafx.scene.text.Text;
 import javafx.scene.text.Font;
 import javafx.scene.layout.VBox;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.GridPane;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.geometry.Pos;
@@ -33,68 +34,88 @@ public class LoskaUI extends Application {
     @Override
     public void start(Stage stage) {
  
-        Maze m = new Maze(4, 7);  
+        Maze m;
+        Button btn1 = new Button("BT"); 
+        Button btn2 = new Button("DFS");
+
+        m = new Maze(10, 10);  
         BinaryTree bt = new BinaryTree(m);
         bt.generate();
-        System.out.println("Binary Tree");
-        System.out.print(bt);
+        //System.out.println("Binary Tree");
+        //System.out.print(bt);
 
         System.out.print("\n\n");
 
-        m = new Maze(7, 7);
+        m = new Maze(10, 10);
         DFS dfs = new DFS(m);
         dfs.generate();
-        System.out.println("Depth-First Search");
-        System.out.println(dfs);
-        
+        //System.out.println("Depth-First Search");
+        //System.out.println(dfs);
+ 
+
+
         // Main scene
         //top row for control buttons
-        HBox control = createTopRow();
+        GridPane control = createTopRow(btn1, btn2);
         
         //maze display box
-        VBox mazeDisplay = createMazeDisplay(dfs);
+        VBox mazeDisplay = createMazeDisplay();
 
         //whole area
         VBox root = new VBox(control, mazeDisplay);
-        Scene scene = new Scene(root, 800, 600);
+        Scene scene = new Scene(root, 800, 600);    
         stage.setScene(scene);
         stage.setTitle("Maze generator - BT + DFS");
 
-
         stage.show();
+
+        btn1.setOnAction(e -> {
+            updateMazeDisplay(bt, mazeDisplay);           
+        });
+ 
+        btn2.setOnAction(e -> {
+            updateMazeDisplay(dfs, mazeDisplay);           
+        });
+
+        //updateMazeDisplay(bt, mazeDisplay);
 
         //System.exit(0);
     }
 
 
-    public HBox createTopRow() {
-        HBox control = new HBox(4);
-        Button btn1 = new Button("BT");
-        Button btn2 = new Button("DFS");
+    public GridPane createTopRow(Button btn1, Button btn2) {
+        GridPane control = new GridPane();
+        //btn1 = new Button("BT");
+        //btn2 = new Button("DFS");
         Text text1 = new Text("height");
         Text text2 = new Text("width");
         TextField height = new TextField("7");
         TextField width = new TextField("7");
 
+        btn1.setMinWidth(50);
+        btn2.setMinWidth(50);
+
         height.setPrefWidth(50);
         width.setPrefWidth(50);
 
-        control.getChildren().add(btn1);
-        control.getChildren().add(btn2);
-        control.getChildren().add(text1);
-        control.getChildren().add(text2);
-        control.getChildren().add(height);
-        control.getChildren().add(width);
+        control.add(text1, 0, 0, 1, 1);
+        control.add(text2, 0, 1, 1, 1);
+        control.add(height, 1, 0, 1, 1);
+        control.add(width,  1, 1, 1, 1);
+        control.add(btn1, 3, 0, 2, 2);
+        control.add(btn2, 5, 0, 2, 2);
 
         control.setPadding(new Insets(50, 50, 50, 50));
-        control.setSpacing(10);
+        control.setAlignment(Pos.CENTER);
+
+        control.setHgap(10);
+        control.setVgap(10);
 
         return control;
     }
 
-    public VBox createMazeDisplay(DFS dfs) {
-        Text text = new Text(dfs.toString());
-        //Text text = new Text("Here be maze");
+    public VBox createMazeDisplay() {
+        Text text = new Text("Here be maze");
         text.setFont(Font.font("Ubuntu Mono", 20));
 
         VBox mazeDisplay = new VBox(text);
@@ -117,8 +138,13 @@ public class LoskaUI extends Application {
     }
 
     //updates mazeDisplay node contents
-    public void updateMazeDisplay() {
+    public void updateMazeDisplay(Object t, VBox vb) {
+        Text text = new Text(t.toString());
+        text.setFont(Font.font("Ubuntu Mono", 20));
 
+        vb.getChildren().clear();
+        vb.getChildren().add(text);
     }
+
 
 }
